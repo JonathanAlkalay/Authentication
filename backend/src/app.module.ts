2e5eeb64e0338160cc4b0f12768779a21bbc7347
Jonathan <alkalayjonathan28@yahoo.com>
2022-11-16 01:07:53 +0200

    added configurations and sequelize

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.MYSQL_HOST ?? 'localhost',
+      port: 3306,
+      username: 'user',
+      password: 'secret',
+      database: 'deathStar',
+      autoLoadModels: true,
+      logging: false,
+      dialectOptions: { decimalNumbers: true },
+      synchronize: false,
+      models: [],
+    }),
+    SequelizeModule.forFeature([
+
+    ])
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,26 @@
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet'
+
+
+const prepareApp = async (): Promise<INestApplication> =>{
+
+  const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({ origin: '*' });
+  app.useGlobalPipes( new ValidationPipe({ transform: true }) );
+  app.use(helmet({ crossOriginEmbedderPolicy: false }));
+
+  return app;
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const logger: Logger = new Logger('NestApplication', { timestamp: true });
+
+  const app = await prepareApp();
+  await app.listen(8080, () => logger.log(`App listening on port 8080 !!!`));
 }
+
 bootstrap();
