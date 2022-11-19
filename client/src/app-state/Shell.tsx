3e226e0be496269676f75c9a2f805dd2595da442
Alon Goldsmith <alongold91@gmail.com>
2022-11-19 20:55:06 +0200

    create login page

--- client/src/app-state/Shell.tsx
+++ client/src/app-state/Shell.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Login from "../log-in/Login"
 
 
 
 export const Shell = () => {
     return (
         <Router>
-
+            <Login/>
         </Router>
     )
 }
