256360d5eda7bc16140c6cc7637abbec8e12aca7
Alon Goldsmith <alongold91@gmail.com>
2022-11-19 20:38:24 +0200

    install packages and configure theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,9 @@
 import {Provider} from 'react-redux'
 import { Shell } from './app-state/Shell';
-import { createStore } from './app-state/Store'
+import { createStore } from './app-state/Store';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@emotion/react';
+import {Theme} from "./common/components/toolkit/Theme";
 
 
 const store = createStore();
@@ -8,7 +11,10 @@ const store = createStore();
 const App = () => {
   return (
     <Provider store={store}>
+      <CssBaseline />
+      <ThemeProvider theme={Theme}>
       <Shell/>
+      </ThemeProvider>
     </Provider>
   )
 }
---
+++ client/src/common/components/toolkit/Theme.tsx
@@ -0,0 +1,19 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+   palette :{
+    primary: {
+        main: "#6495ED"
+    },
+    secondary : {
+        main: "#EE4B2B"
+    },
+    background: {
+        default: "#D3D3D3",                  
+    },
+    text: {
+        primary: "#301934",
+        disabled: "#483248"
+    }
+   }
+  });
