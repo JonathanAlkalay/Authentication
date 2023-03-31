4edd35b5c5a20c09b82527a47ef16a1e38e460f3
Jonathan <alkalayjonathan28@yahoo.com>
2023-03-31 19:12:02 +0300

    gave button sizes

--- client/src/common/components/non-formik/buttons/BaseButton.tsx
+++ client/src/common/components/non-formik/buttons/BaseButton.tsx
@@ -1,14 +1,19 @@
+import { SxProps } from '@mui/material';
 import Button from '@mui/material/Button';
 
+const styles: SxProps = {
+    height: '45px',
+    width: '120px'
+}
 interface BaseButtonProps {
     label: string;
-    style: {};
+    style: SxProps;
     onClick: () => void;
     variant: "text" | "outlined" | "contained";
     disabled?: boolean;
 }
 export const BaseButton = ({label, style, onClick, variant, disabled}: BaseButtonProps) => {
     return (
-        <Button disabled={disabled ?? false} variant={variant} sx={style} onClick={onClick}> { label } </Button>
+        <Button disabled={disabled ?? false} variant={variant} sx={{...styles ,...style}} onClick={onClick}> { label } </Button>
     )
 }
